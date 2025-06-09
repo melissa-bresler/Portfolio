cbf1832bb90c15050e3659b24f703669a941627c
melissa-bresler <108290826+melissa-bresler@users.noreply.github.com>
2025-06-09 13:14:01 +0100

    Restyle About Page
    Adds images and styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,34 +1,43 @@
 import React from "react";
 import { Container, Typography, Box } from "@mui/material";
+import photo from "../assets/about-me-photo.jpg";
+import machine from "../assets/arcade-machine.png";
+import styles from "../styles/About.module.css";
 
 const About: React.FC = () => {
   return (
-    <Container sx={{ marginTop: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        About Me
-      </Typography>
-      <Box sx={{ maxWidth: "95%", margin: "0 auto", padding: 2 }}>
-        <Typography variant="body1" component="div" sx={{ lineHeight: 1.8 }}>
-          <p>
-            I’m a software developer based in the UK with a strong interest in
-            game development. Outside of my day job, I’ve been teaching myself
-            how to design and build games—starting with small side projects and
-            gradually picking up the skills involved along the way.
-          </p>
-          <p>
-            This site is where I share what I’ve worked on so far. It’s a mix of
-            finished games and work in progress, along with some notes on how
-            things are going. I enjoy experimenting with mechanics, exploring
-            new tools, and figuring out how to make something that’s fun to
-            play.
-          </p>
-          <p>
-            It’s an ongoing process, but one I really enjoy and want to keep
-            building on. Thanks for taking a look.
-          </p>
-        </Typography>
-      </Box>
-    </Container>
+    <div>
+      <img src={machine} alt="arcade machine" className={styles.static} />
+      <Container sx={{ marginTop: 4 }}>
+        <Box sx={{ maxWidth: "60%", margin: "0 auto", padding: 2 }}>
+          <img src={photo} alt="photo" className={styles.image} />
+          <Typography
+            variant="body1"
+            component="div"
+            sx={{ lineHeight: 1.8 }}
+            align="justify"
+          >
+            <p>
+              I’m a software developer based in the UK with a strong interest in
+              game development. Outside of my day job, I’ve been teaching myself
+              how to design and build games—starting with small side projects
+              and gradually picking up the skills involved along the way.
+            </p>
+            <p>
+              This site is where I share what I’ve worked on so far. It’s a mix
+              of finished games and work in progress, along with some notes on
+              how things are going. I enjoy experimenting with mechanics,
+              exploring new tools, and figuring out how to make something that’s
+              fun to play.
+            </p>
+            <p>
+              It’s an ongoing process, but one I really enjoy and want to keep
+              building on. Thanks for taking a look.
+            </p>
+          </Typography>
+        </Box>
+      </Container>
+    </div>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,6 +17,7 @@ const Home: React.FC = () => {
     {
       title: "Memory Meltdown",
       description:
+        "Co-developed by James Watson, Joao Tiago Da Silva Figueira, and myself, Memory Meltdown is a mobile game that tests your memorization skills through three fun mini-games.",
       link: "/memory-meltdown",
       image: mmLogo,
     },
@@ -31,17 +32,6 @@ const Home: React.FC = () => {
 
   return (
     <div className={styles.homeContainer}>
-      {/* <Typography variant="h2" gutterBottom>
-        Welcome to My Portfolio
-      </Typography>
-      <Typography variant="body1">
-        This is the home page. Use the dropdown under "Home" in the navigation
-        bar to access different sections.
-      </Typography> */}
-
-      {/* <Typography variant="h4" sx={{ marginTop: 4 }} gutterBottom>
-        Games
-      </Typography> */}
       <GameCarousel games={games} />
     </div>
   );
